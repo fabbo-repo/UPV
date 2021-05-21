80e40c86f7f3fc85a3123c66e45e740b6284f230
Fabián Scherle <47057180+FabboBox@users.noreply.github.com>
2021-05-21 16:52:01 +0200

    Add files via upload

---
+++ AppGym/ProyectoPracticas/ClassLibrary/BusinessLogic/Entities/Gym.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestDep.Entities
+{
+    public partial class Gym
+    {
+        //Constructor
+        public Gym () {
+            this.Activities = new List<Activity>();
+            this.Rooms = new List<Room>();
+        }
+
+        public Gym (DateTime closingHour, int discountLocal, int discountRetired, 
+            double freeUserPrice, string name, DateTime openingHour, int zipCode):this() {
+            this.ClosingHour = closingHour;
+            this.DiscountLocal = discountLocal;
+            this.DiscountRetired = discountRetired;
+            this.FreeUserPrice = freeUserPrice;
+            //EF se encarga de los atributos ID de tipo int
+            this.Name = name;
+            this.OpeningHour = openingHour;
+            this.ZipCode = zipCode;
+        }
+
+        public void AddActivity(Activity a)
+        {
+            this.Activities.Add(a);
+        }
+        public void AddRoom(Room r)
+        {
+            this.Rooms.Add(r);
+        }
+
+        public Activity GetActivityById(int ActivityId)
+        {
+            foreach (Activity ac in this.Activities)
+            {
+                if (ActivityId == ac.Id) { return ac; }
+            }
+            return null;
+        }
+        public Room GetRoomById(int roomId) 
+        {
+            foreach (Room r in this.Rooms)
+                if (r.Id == roomId) { return r; }
+            return null;
+        }
+        public Payment GetPaymentById(int paymentId)
+        {
+            foreach (Activity ac in this.Activities)
+            {
+                Payment p = ac.GetPaymentById(paymentId);
+                if (p != null) return p;
+            }
+            return null;
+        }
+
+        public ICollection<int> GetAllActivitiesIds()
+        { 
+            ICollection<int> ac = new List<int>();
+            foreach (Activity a in this.Activities) { ac.Add(a.Id); }
+            return ac;
+        }
+        public ICollection<int> GetAllRoomsIds()
+        {
+            ICollection<int> room = new List<int>();
+            foreach (Room rm in this.Rooms)
+            {
+                room.Add(rm.Id);
+            }
+            return room;
+        }
+
+        public ICollection<Room> GetAllRoomsFromIds(ICollection<int> ids)
+        {
+            ICollection<Room> room = new List<Room>();
+            foreach (int id in ids)
+            {
+                Room r = this.GetRoomById(id);
+                if(r!=null) room.Add(r);
+            }
+            return room;
+        }
+
+        public ICollection<int> GetAllRunningOrFutureActivitiesIds()
+        {
+            ICollection<int> act = new List<int>();
+            foreach (Activity a in this.Activities) { 
+                if(a.IsRunningOrFuture())act.Add(a.Id); }
+            return act;
+        }
+
+        public ICollection<int> GetListAvailableRoomsIds(Days activityDays, TimeSpan duration, DateTime finishDate, DateTime startDate, DateTime startHour)
+        {
+            ICollection<int> rooms = new List<int>();
+            foreach (Room r in this.Rooms)
+                if (r.IsAvailable(activityDays, duration, finishDate, startDate, startHour))
+                    rooms.Add(r.Id);
+
+            return rooms;
+        }
+
+        public double GetFirstQuotaByActivityId(int activityId, Boolean retired, int zipCode)
+        {
+            Activity ac = GetActivityById(activityId);
+            if (ac == null) throw new ArgumentException("Error: No existe esta actividad.");
+            double cuota = ac.Price;
+            if (retired && zipCode == this.ZipCode)
+            {
+                cuota = (100 - this.DiscountLocal - this.DiscountRetired) * cuota / 100;
+            }
+            else if (zipCode == this.ZipCode) { cuota = (100 - this.DiscountLocal) * cuota / 100; }
+            else if (retired) { cuota = (100 - this.DiscountRetired) * cuota / 100; }
+            return cuota;
+        }
+
+        public void GetActivityDataFromId(int ActivityId, out Days activityDays, out string description, out TimeSpan duration, out DateTime finishDate, 
+            out int maximumEnrollments, out int minimumEnrollments, out double price, out DateTime startDate, out DateTime startHour, 
+            out ICollection<int> enrollmentIds, out string instructorId, out ICollection<int> roomIds)
+        {
+            Activity a = this.GetActivityById(ActivityId);
+            if (a == null) throw new ArgumentException("Error: No existe esta actividad.");
+            a.GetActivityData(out activityDays, out description, out duration, out finishDate, out maximumEnrollments, out minimumEnrollments, out price,
+                out startDate, out startHour, out enrollmentIds, out instructorId, out roomIds);
+        }
+
+        public void GetRoomDataFromId(int roomId, out int number, out ICollection<int> activityIds)
+        {
+            Room room = this.GetRoomById(roomId);
+            if (room == null) throw new ArgumentException("Error: No existe la sala.");
+            room.GetRoomData(out number, out activityIds);
+        }
+
+        public void GetPaymentDataFromId(int paymentId, out DateTime date, out string description, out double quantity)
+        {
+            Payment p = this.GetPaymentById(paymentId); //Esta función se encuentra en Gym, Activity y Enrollment.
+            if (p == null) throw new ArgumentException("Error: No existe este pago.");
+            p.GetPaymentData(out date, out description, out quantity);
+        }
+
+        public void GetEnrollmentDataFromIds(int activityId, int enrollmentId, out DateTime? cancellationDate, out DateTime enrollmentDate, 
+            out DateTime? returnedFirstCuotaIfCancelledActivity, out ICollection<int> paymentIds, out string userId)
+        {
+            Activity ac = this.GetActivityById(activityId);
+            if (ac == null) { throw new ArgumentException("Error: No existe esta actividad."); }
+            ac.GetEnrollmentDataFromId(enrollmentId, out cancellationDate, out enrollmentDate, out returnedFirstCuotaIfCancelledActivity, 
+                out paymentIds, out userId);
+        }
+
+        public void GetGymData(out int gymId, out DateTime closingHour, out int discountLocal, out int discountRetired, out double freeUserPrice, out string name, out DateTime openingHour, out int zipCode, out ICollection<int> activityIds, out ICollection<int> roomIds)
+        {
+            gymId = this.Id; closingHour = this.ClosingHour; discountLocal = this.DiscountLocal; discountRetired = this.DiscountRetired;
+            freeUserPrice = this.FreeUserPrice; name = this.Name; openingHour = this.OpeningHour;
+            zipCode = this.ZipCode; activityIds = this.GetAllActivitiesIds(); roomIds = this.GetAllRoomsIds();
+        }
+
+        public int AddNewActivity(Days activityDays, string description, TimeSpan duration, DateTime finishDate, int maximumEnrollments,
+            int minimumEnrollments, double price, DateTime startDate, DateTime startHour, ICollection<int> roomsIds)
+        {
+            if (maximumEnrollments < 0)
+                throw new ArgumentException("Error: Máximo de inscripciones no debe ser menor a 0.");
+            if (minimumEnrollments < 0)
+                throw new ArgumentException("Error: Mínimo de inscripciones no debe ser menor a 0.");
+            if (minimumEnrollments > maximumEnrollments)
+                throw new ArgumentException("Error: Máximo o mínimo inscripciones invalidas.");
+            if (price < 0)
+                throw new ArgumentException("Error: Precio no debe ser menor a 0.");
+            if(roomsIds==null)
+                throw new ArgumentException("Error: Precio no debe ser menor a 0.");
+            
+           ICollection<Room> rooms = this.GetAllRoomsFromIds(roomsIds);
+            if(rooms.Count() == 0)
+                throw new ArgumentException("Error: No hay salas seleccionadas existentes.");
+
+            foreach (Room r in rooms)
+            {
+                if (!r.IsAvailable(activityDays, duration, finishDate, startDate, startHour))
+                    throw new ArgumentException("Error: No estan disponibles las salas.");
+            }
+            
+            Activity a = new Activity(activityDays, description, duration, finishDate, maximumEnrollments, minimumEnrollments, price,
+               startDate, startHour);
+
+            a.AddRooms(rooms);
+            foreach (Room r in rooms)
+            {
+                r.AddActivity(a);
+            }
+            this.AddActivity(a);
+            
+            return a.Id;
+        }
+
+        public void AssignInstructorToActivity(int activityId, Instructor instructor)
+        {
+            Activity a = this.GetActivityById(activityId);
+            if (a == null) throw new ArgumentException("Error: Esta actividad no existe en la BD.");
+            if (instructor == null) throw new ArgumentException("Error: Instructor no existe.");
+            if (!instructor.IsAvailable(a.ActivityDays, a.Duration, a.FinishDate, a.StartDate, a.StartHour)) 
+                throw new ArgumentException("Error: Este instructor no está disponible.");
+            
+            a.AddInstructor(instructor);
+            instructor.AddActivity(a);
+        }
+
+        public void EnrollUserInActivity(int activityId, User user, out Enrollment enrolm, out Payment paym)
+        {
+            if (user == null) throw new ArgumentException("Error: Usuario no existe.");
+            Activity a = this.GetActivityById(activityId);
+            if (a == null) throw new ArgumentException("Error: Actividad no existe.");
+            if(user.IsEnrolledInActivityId(activityId)) throw new ArgumentException("Error: Usuario ya está inscrito en la actividad.");
+
+            double pago = this.GetFirstQuotaByActivityId(activityId, user.Retired, user.ZipCode);
+            Payment p = new Payment(DateTime.Today,"Usuario "+user.Name+", ha pagado "+pago, pago);
+            Enrollment en = new Enrollment(DateTime.Today,a,p,user);
+            a.AddEnrollment(en); user.AddEnrollment(en);
+            enrolm = en;
+            paym = p;
+        }
+
+        public Days ToDays(DayOfWeek DayOfWeek)
+        {
+            Days dia;
+            switch (DayOfWeek)
+            {
+                case DayOfWeek.Monday: dia = Days.Mon; break;
+                case DayOfWeek.Tuesday: dia = Days.Tue; break;
+                case DayOfWeek.Wednesday: dia = Days.Wed; break;
+                case DayOfWeek.Thursday: dia = Days.Thu; break;
+                case DayOfWeek.Friday: dia = Days.Fri; break;
+                case DayOfWeek.Saturday: dia = Days.Sat; break;
+                case DayOfWeek.Sunday: dia = Days.Sun; break;
+                default: dia = Days.None; break;
+            }
+            return dia;
+        }
+
+        public int NumAvailableRooms(Days activityDays, TimeSpan duration, DateTime finishDate, DateTime startDate, DateTime startHour) 
+        {
+            int cont = GetListAvailableRoomsIds(activityDays, new TimeSpan(0,45,0), finishDate, startDate, startHour).Count();
+            return cont;
+        }
+
+        public Dictionary<DateTime, int> GetListAvailableRoomsPerWeek(DateTime initialMonday)
+        {
+            // Devuelve pares dia y numero de habitaciones disponibles, de todos los dias de la semana desde la fecha del initialMonday 
+            Dictionary<DateTime, int> roomsPerTime = new Dictionary<DateTime, int>();
+            DateTime endSunday = initialMonday.AddDays(6);
+            for (DateTime fecha = initialMonday; fecha.CompareTo(endSunday) <= 0; fecha=fecha.AddDays(1))
+            {
+                for (DateTime hora = this.OpeningHour; hora.CompareTo(this.ClosingHour) < 0; hora=hora.AddMinutes(45))
+                {
+                    int cont = this.NumAvailableRooms(this.ToDays(fecha.DayOfWeek), new TimeSpan(0, 45, 0), fecha, fecha, hora);
+                    DateTime fechaD = new DateTime(fecha.Year, fecha.Month, fecha.Day, hora.Hour, hora.Minute, hora.Second); 
+                    roomsPerTime.Add(fechaD, cont);
+                }
+            }
+            return roomsPerTime;
+        }
+    }
+}
---
+++ AppGym/ProyectoPracticas/ClassLibrary/BusinessLogic/Services/IGestDepService.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GestDep.Entities;
+
+namespace GestDep.Services
+{
+    public interface IGestDepService
+    {
+        
+        #region Add new activity
+        /// <summary>
+        /// Adds a news activity
+        /// </summary>
+        /// <param name="activityDays"></param>
+        /// <param name="description"></param>
+        /// <param name="duration"></param>
+        /// <param name="finishDate"></param>
+        /// <param name="maximumEnrollments"></param>
+        /// <param name="minimumEnrollments"></param>
+        /// <param name="price"></param>
+        /// <param name="startDate"></param>
+        /// <param name="startHour"></param>
+        /// <param name="roomsIds"></param>
+        /// <returns>Id of the new activity</returns>
+        /// <exception cref="ServiceException">Thrown when no available room exists, the date and time ranges are not valid, or the activity is programmed to start before now.</exception> 
+        int AddNewActivity(Days activityDays, String description, TimeSpan duration, DateTime finishDate,
+            int maximumEnrollments, int minimumEnrollments, double price,
+            DateTime startDate, DateTime startHour, ICollection<int> roomsIds);
+
+        /// <summary>
+        /// Lists all rooms that are available for use in the given time frame.
+        /// </summary>
+        /// <param name="activityDays"></param>
+        /// <param name="startDate"></param>
+        /// <param name="finishDate"></param>
+        /// <param name="startHour"></param>
+        /// <param name="finishHour"></param>
+        /// <returns>A collection of room ids</returns>
+        /// <exception cref="ServiceException">Thrown when no available room exists or the date and time ranges are not valid.</exception>
+        ICollection<int> GetListAvailableRoomsIds(Days activityDays, TimeSpan duration, DateTime finishDate, DateTime startDate, DateTime startHour);
+
+        /// <summary>
+        /// Gets the room data for the given id.
+        /// </summary>
+        /// <param name="roomId"></param>
+        /// <param name="number"></param>
+        /// <param name="activityIds">If there are no activities scheduled to take place in the room, returns an empty ICollection of int.</param>
+        /// <exception cref="ServiceException">Thrown when the room doesn't exist.</exception>
+        void GetRoomDataFromId(int roomId, out int number, out ICollection<int> activityIds);
+        #endregion
+        #region Assign instructor to activity.
+        /// <summary>
+        /// Gets the activity data for the given id.
+        /// </summary>
+        /// <param name="ActivityId"></param>
+        /// <param name="activityDays"></param>
+        /// <param name="description"></param>
+        /// <param name="duration"></param>
+        /// <param name="finishDate"></param>
+        /// <param name="maximumEnrollments"></param>
+        /// <param name="minimumEnrollments"></param>
+        /// <param name="price"></param>
+        /// <param name="startDate"></param>
+        /// <param name="startHour"></param>
+        /// <param name="enrollmentIds">If no enrollment has been added, returns an empty list.</param>
+        /// <param name="instructorId">If no instructor has been set, returns an empty string.</param>
+        /// <param name="roomIds">If no room has been added, returns an empty list.</param>
+        /// <exception cref="ServiceException">Thrown when the activity doesn't exist.</exception> 
+        void GetActivityDataFromId(int ActivityId, out Days activityDays, out String description, out TimeSpan duration, 
+            out DateTime finishDate, out int maximumEnrollments, out int minimumEnrollments, out double price,
+            out DateTime startDate, out DateTime startHour, out ICollection<int> enrollmentIds, 
+            out String instructorId, out ICollection<int> roomIds);
+
+        /// <summary>
+        /// Gets all activities ids.
+        /// </summary>
+        /// <returns>A list of activity ids.</returns>
+        ICollection<int> GetAllActivitiesIds();
+
+
+        /// <summary>
+        /// Gets the instructor data for the given id.
+        /// </summary>
+        /// <param name="instructorId"></param>
+        /// <param name="address"></param>
+        /// <param name="IBAN"></param>
+        /// <param name="name"></param>
+        /// <param name="zipCode"></param>
+        /// <param name="ssn"></param>
+        /// <param name="activitiesIds">If the instructor doesn't teach any activity, returns an empty ICollection of int</param>
+        /// <exception cref="ServiceException">Thrown when the instructor doesn't exist.</exception>
+        void GetInstructorDataFromId(string instructorId, out String address, out String IBAN, out String name, out int zipCode,
+            out String ssn, out ICollection<int> activitiesIds);
+
+        /// <summary>
+        /// Gets all instructors that are available in the given time frame.
+        /// </summary>
+        /// <param name="activityDays"></param>
+        /// <param name="startDate"></param>
+        /// <param name="finishDate"></param>
+        /// <param name="startHour"></param>
+        /// <returns>A list of instructor ids (string)</returns>
+        /// <exception cref="ServiceException">Thrown when there are not available instructors or the date and time ranges are not valid.</exception> 
+        ICollection<string> GetAvailableInstructorsIds(Days activityDays, TimeSpan duration, DateTime finishDate, DateTime startDate, DateTime startHour);
+
+        /// <summary>
+        /// Assigns an instructor to an activity
+        /// </summary>
+        /// <param name="activityId"></param>
+        /// <param name="instructorId"></param>
+        /// <exception cref="ServiceException">Thrown when either the instructor id or activity id don't exist.</exception>
+        void AssignInstructorToActivity(int activityId, string instructorId);
+        #endregion
+        #region Enroll into activity
+        /// <summary>
+        /// Gets all activities that are currently running or will run in the future.
+        /// </summary>
+        /// <returns>A list of actitity ids</returns>
+        /// <exception cref="ServiceException">Thrown when there are not activities planned or running.</exception> 
+        ICollection<int> GetAllRunningOrFutureActivitiesIds();
+
+        /// <summary>
+        /// Gets all user data for the given id.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="address"></param>
+        /// <param name="iban"></param>
+        /// <param name="name"></param>
+        /// <param name="zipCode"></param>
+        /// <param name="birthDate"></param>
+        /// <param name="retired"></param>
+        /// <param name="enrollmentIds">If the user is not enrolled in any activity, returns an empty ICollection of int.</param>
+        /// <exception cref="ServiceException">Thrown when there are no users with this Id.</exception> 
+        void GetUserDataFromId(string userId, out string address, out string iban, out string name, out int zipCode,
+            out DateTime birthDate, out bool retired, out ICollection<int> enrollmentIds);
+
+        /// <summary>
+        /// Gets all user data for the given id, but also checks that the user is no already enrolled in that activity.
+        /// </summary>
+        /// <param name="activityId"></param>
+        /// <param name="userId"></param>
+        /// <param name="address"></param>
+        /// <param name="iban"></param>
+        /// <param name="name"></param>
+        /// <param name="zipCode"></param>
+        /// <param name="birthDate"></param>
+        /// <param name="retired"></param>
+        /// <param name="enrollmentIds">If the user is not enrolled in any activity, returns an empty ICollection of int.</param>
+        /// <returns>Returns the first quota</returns>
+        /// <exception cref="ServiceException">Thrown when the user is already enrolled in the activity or the user or the activity don't exist (requires two different message exceptions).</exception>
+        double GetUserDataNotInActivityAndFirstQuota(int activityId, string userId, out string address, out string iban, 
+            out string name, out int zipCode,
+            out DateTime birthDate, out bool retired, out ICollection<int> enrollmentIds);
+
+        /// <summary>
+        /// Enrolls a user in an activity.
+        /// </summary>
+        /// <param name="activityId"></param>
+        /// <param name="userId"></param>
+        /// <returns>Returns the id of the generated enrollment.</returns>
+        /// <exception cref="ServiceException">Thrown when either the user id or activity id don't exist.</exception>
+        int EnrollUserInActivity(int activityId, string userId);
+
+        /// <summary>
+        /// Gets all enrollment data for the given activity and enrollment ids
+        /// </summary>
+        /// / <param name="activityId"></param>
+        /// <param name="enrollmentId"></param>
+        /// <param name="cancellationDate"></param>
+        /// <param name="enrollmentDate"></param>
+        /// <param name="returnedFirstCuotaIfCancelledActivity"></param>
+        /// <param name="activityId"></param>
+        /// <param name="paymentIds"></param>
+        /// <param name="userId"></param>
+        /// <exception cref="ServiceException">Thrown when the enrollment doesn't exist.</exception> 
+        void GetEnrollmentDataFromIds(int activityId, int enrollmentId, out DateTime? cancellationDate, out DateTime enrollmentDate,
+            out DateTime? returnedFirstCuotaIfCancelledActivity, out ICollection<int> paymentIds, 
+            out string userId);
+
+        /// <summary>
+        /// Gets all payment data for the given id.
+        /// </summary>
+        /// <param name="paymentId"></param>
+        /// <param name="date"></param>
+        /// <param name="description"></param>
+        /// <param name="quantity"></param>
+        /// <exception cref="ServiceException">Thrown when the payment doesn't exist.</exception> 
+        void GetPaymentDataFromId(int paymentId, out DateTime date, out string description, out double quantity);
+        #endregion
+        #region Add new user
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="iban"></param>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="zipCode"></param>
+        /// <param name="birthDate"></param>
+        /// <param name="retired"></param>
+        /// <exception cref="ServiceException">Thrown when the user already exists or when the provided data is not valid (for instance, the birth date is after the current instant).</exception> 
+        void AddNewUser(string address, string iban, string id, string name, int zipCode, DateTime birthDate, bool retired);
+        #endregion
+        #region List free rooms in week.
+        /// <summary>
+        /// Gets all available rooms per time frame per week.
+        /// </summary>
+        /// <param name="initialMonday"></param>
+        /// <returns>A dictionary with all time slots in the week with the number of rooms available at that moment.</returns>
+        /// <exception cref="ServiceException">Thrown when the selected date is not a monday.</exception>
+        Dictionary<DateTime, int> GetListAvailableRoomsPerWeek(DateTime initialMonday);
+        
+        /// <summary>
+        /// Returns the data of the gym of the CityHall (services work with only one gym)
+        /// </summary>
+        /// <param name="gymId"></param>
+        /// <param name="closingHour"></param>
+        /// <param name="discountLocal"></param>
+        /// <param name="discountRetired"></param>
+        /// <param name="freeUserPrice"></param>
+        /// <param name="name"></param>
+        /// <param name="openingHour"></param>
+        /// <param name="zipCode"></param>
+        /// <param name="activityIds">If no activity has been added, returns an empty ICollection of int</param>
+        /// <param name="roomIds">If no room has been added, returns an empty ICollection of int</param>
+        /// <exception cref="ServiceException">Thrown when the cityHall has no gym.</exception>
+        void GetGymData( out int  gymId, out DateTime closingHour, out int discountLocal, out int discountRetired, out double freeUserPrice,
+                    out String name, out DateTime openingHour, out int zipCode, out ICollection<int> activityIds, out ICollection<int> roomIds);
+
+        #endregion
+        #region Others
+        /// <summary>
+        /// Removes all data from the database.
+        /// </summary>
+        void RemoveAllData();
+
+        /// <summary>
+        /// Saves all modified entities.
+        /// </summary>
+        void SaveChanges();
+        #endregion
+    }
+}
